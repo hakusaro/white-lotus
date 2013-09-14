2143ea26699faa7e3e588f82a8c932dea1ef8a7c
Lucas Nicodemus <shank@shanked.me>
2013-09-14 00:45:02 -0600

    Add base routes/initializer

---
+++ app.rb
@@ -0,0 +1,38 @@
+#coding: utf-8
+
+require 'sinatra'
+require 'sinatra/session'
+require 'omniauth'
+require 'omniauth-openid'
+require 'pry'
+require 'openid/store/filesystem'
+require 'awesome_print'
+require 'yaml'
+
+if (ENV['environment'] ? ENV['environment'].to_sym : :dev == :production) then
+  puts 'Server is running in production mode but nobody put anything different here.'
+  set :environment => :production
+else
+  set :environment => :development
+end
+
+if (settings.environment == :production) then
+  set :logging => false,
+    :dump_errors => false,
+    :raise_errors => false
+else
+  set :logging => true,
+    :dump_errors => true,
+    :raise_errors => true,
+    :session_secret => 'maka_is_not_a_secret',
+    :bind => '0.0.0.0',
+    :port => 4567
+end
+
+set :partial_template_engine => :erb
+
+use OmniAuth::Builder do
+  provider :open_id, :name => 'steam', 'identifier' => 'http://steamcommunity.com/openid'
+end
+
+require_relative 'routes/init'
---
+++ routes/index.rb
@@ -0,0 +1,3 @@
+get '/*' do
+  "Noop"
+end
