a8d39826dea3f3b73a98252c4baa365401f6c3c8
Zack Piispanen <olink@shankshock.com>
2013-09-14 22:12:12 -0400

    Added TShock Plugin

---
+++ white-lotus-tshock-plugin/WhiteLotus/UserManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace WhiteLotus
+{
+    internal class UserManager
+    {
+        private readonly IDbConnection database;
+
+        public UserManager(IDbConnection db)
+        {
+            database = db;
+            var table = new SqlTable("users",
+                new SqlColumn("steam64", MySqlDbType.VarChar, 64),
+                new SqlColumn("username", MySqlDbType.VarChar, 32) {Primary = true, Unique = true},
+                new SqlColumn("banned", MySqlDbType.Int32)
+                );
+            var creator = new SqlTableCreator(db,
+                db.GetSqlType() == SqlType.Sqlite
+                    ? (IQueryBuilder) new SqliteQueryCreator()
+                    : new MysqlQueryCreator());
+            creator.EnsureExists(table);
+        }
+
+        public void InsertUser(string steamid, string accountname)
+        {
+            try
+            {
+                if (
+                    database.Query("INSERT INTO users (steam64, username, banned) VALUES (@0, @1, @2);", steamid,
+                        accountname, 0) != 1)
+                {
+                    throw new UserException(string.Format("User {0} already exists.", accountname));
+                }
+            }
+            catch (Exception e)
+            {
+                if (Regex.IsMatch(e.Message, "username.*not unique"))
+                    throw new UserException(string.Format("User {0} already exists.", accountname));
+                throw new UserException("InsertUser SQL returned an error (" + e.Message + ")", e);
+            }
+        }
+
+        public List<SteamUser> GetUserAccounts(string steam64)
+        {
+            List<SteamUser> accounts = new List<SteamUser>();
+            using (var reader = database.QueryReader("SELECT * FROM users WHERE steam64 = @0", steam64))
+            {
+                while (reader.Read())
+                {
+                    SteamUser user = new SteamUser();
+                    user.Steam64 = reader.Get<String>("steam64");
+                    user.UserAccountName = reader.Get<String>("username");
+                    user.Banned = (reader.Get<Int32>("banned") != 0);
+
+                    accounts.Add(user);
+                }
+            }
+
+            return accounts;
+        }
+
+    }
+
+    class SteamUser
+    {
+        public String Steam64 { get; set; }
+        public String UserAccountName { get; set; }
+        public bool Banned { get; set; }
+    }
+
+    class UserException : Exception
+    {
+        public UserException(string m) : base(m)
+        {
+        }
+
+        public UserException(string m, Exception e)
+            : base(m, e)
+        {
+        }
+    }
+}
---
+++ white-lotus-tshock-plugin/WhiteLotus/WhiteLotus.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpServer;
+using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
+using Rests;
+using Terraria;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace WhiteLotus
+{
+    [APIVersion(1, 13)]
+    public class WhiteLotus : TerrariaPlugin
+    {
+        private UserManager userManager;
+        public WhiteLotus(Main game) : base(game)
+        {
+            Order = 10;
+        }
+
+        public override string Author
+        {
+            get
+            {
+                return "Nyx Studios";
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "White Lotus";
+            }
+        }
+
+        public override Version Version
+        {
+            get
+            {
+                return new Version(1, 0, 0, 0);
+            }
+        }
+
+        public override void Initialize()
+        {
+            userManager = new UserManager(SetupDB());
+            
+            TShock.RestApi.Register(new SecureRestCommand("/steam/user/add", AddUser, "white-lotus"));
+            TShock.RestApi.Register(new SecureRestCommand("/steam/user/get", GetAccountsForSteam64, "white-lotus"));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+            base.Dispose(disposing);
+        }
+
+        private IDbConnection SetupDB()
+        {
+            IDbConnection db;
+            if (TShock.Config.StorageType.ToLower() == "sqlite")
+            {
+                string sql = Path.Combine(TShock.SavePath, "whitelotus.sqlite");
+                db = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
+            }
+            else if (TShock.Config.StorageType.ToLower() == "mysql")
+            {
+                try
+                {
+                    var hostport = TShock.Config.MySqlHost.Split(':');
+                    db = new MySqlConnection();
+                    db.ConnectionString =
+                        String.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+                                      hostport[0],
+                                      hostport.Length > 1 ? hostport[1] : "3306",
+                                      "whitelotus",
+                                      TShock.Config.MySqlUsername,
+                                      TShock.Config.MySqlPassword
+                            );
+                }
+                catch (MySqlException ex)
+                {
+                    Log.Error(ex.ToString());
+                    throw new Exception("MySql not setup correctly");
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid storage type");
+            }
+            return db;
+        }
+
+        private object AddUser(RestVerbs verbs, IParameterCollection parameters, SecureRest.TokenData tokenData)
+        {
+            string steamid = parameters["steamid"];
+            string accountname = parameters["username"];
+
+            if (string.IsNullOrWhiteSpace(steamid))
+            {
+                return RestMissingParam("steamid");
+            }
+
+            if (string.IsNullOrWhiteSpace(accountname))
+            {
+                return RestMissingParam("username");
+            }
+
+            try
+            {
+                userManager.InsertUser(steamid, accountname);
+            }
+            catch (UserException e)
+            {
+                return RestError(e.Message);
+            }
+
+            return new RestObject("200"){Response = "Successfully added user"};
+        }
+
+        private object GetAccountsForSteam64(RestVerbs verbs, IParameterCollection parameters, SecureRest.TokenData tokenData)
+        {
+            string steamid = parameters["steamid"];
+
+            if (string.IsNullOrWhiteSpace(steamid))
+            {
+                return RestMissingParam("steamid");
+            }
+
+            var accounts = new List<SteamUser>();
+            try
+            {
+                accounts = userManager.GetUserAccounts(steamid);
+            }
+            catch (UserException e)
+            {
+                return RestError(e.Message);
+            }
+
+            return new RestObject("200") {{"users", accounts}};
+        }
+
+        private RestObject RestMissingParam(string var)
+        {
+            return RestError("Missing or empty " + var + " parameter");
+        }
+
+        private RestObject RestError(string message, string status = "400")
+        {
+            return new RestObject(status) { Error = message };
+        }
+    }
+}
